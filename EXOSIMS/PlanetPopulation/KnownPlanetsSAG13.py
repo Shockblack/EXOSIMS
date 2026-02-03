5e2ca6d284a9960914f30e1f64d262e03da555e5
Aiden Zelakiewicz <aidenzelak@gmail.com>
2026-02-03 16:42:04 -0500

    Added new modules for sampled planet population and SimUniverse

---
+++ EXOSIMS/PlanetPopulation/KnownPlanetsSAG13.py
@@ -0,0 +1,184 @@
+from EXOSIMS.PlanetPopulation.SAG13 import SAG13
+from EXOSIMS.util import getExoplanetArchive as nea
+from EXOSIMS.util._numpy_compat import copy_if_needed
+from EXOSIMS.util.InverseTransformSampler import InverseTransformSampler
+from scipy.stats import gaussian_kde
+import numpy as np
+import astropy.units as u
+import astropy.constants as const
+
+class KnownPlanetsSAG13(SAG13):
+    """
+    Planet population that combines both confirmed exoplanets from the NASA Exoplanet Archive (NEA)
+    and synthetic planets. The final distribution of confirmed and synthetic planets follows the
+    SAG13 occurence rates from Kepler.
+
+    This class inherits from the SAG13 class and provides methods to approximate the probability density
+    functions for the NEA data. In general, the final PDF we wish to sample from is:
+        f_sample = (N_tot / N_sampled) * f_target - (N_nea / N_sampled) * f_nea
+
+    """
+
+    def __init__(self, **specs):
+        """
+        Initialize the planetSample class.
+
+        Parameters
+        ----------
+        specs : dict
+            Dictionary of specifications for the planet population.
+        """
+        super().__init__(**specs)
+        # Additional initialization code can go here if needed.
+        
+        SAG13coeffs=[[0.38, -0.19, 0.26, 0.0], [0.73, -1.18, 0.59, 3.4]]
+        SAG13starMass=1.0 * u.Msun
+        
+        mu = const.G * SAG13starMass
+        SAG13coeffs = np.array(SAG13coeffs, dtype=float)
+        SAG13coeffs = SAG13coeffs.T
+        SAG13coeffs = SAG13coeffs[:, np.argsort(SAG13coeffs[3, :])]
+
+        m = mu.to("AU3/year2").value
+
+
+        # Create a dictionary of ranges for the parameters
+        self.NEA_ranges = {
+            'pl_orbsmax': self.arange,
+            'pl_rade':self.Rprange,
+            }
+        
+        self.archive = self.generate_NEA_planets()
+    
+        self.N_nea = len(self.archive['pl_orbsmax'])
+        self.hostname = self.archive['hostname']
+
+        # Placeholder
+        self.planmask = None
+        
+        # Get inferred distributions from NEA data
+        self.dist_rad_nea = lambda Rp: self.infer_PDF(Rp, self.Rprange.value, self.archive['pl_rade'])
+        self.dist_sma_nea = lambda a: self.infer_PDF(a, self.arange.value, self.archive['pl_orbsmax'])
+
+
+
+        # Semi-major axis distributions given radius for two cases
+        self.f_sma_given_Rp1 = lambda a, beta=self.beta[0], m=m, C=self.Ca[0], \
+            smaknee=self.smaknee: self.dist_sma_given_radius(a, beta, m, C, smaknee)
+        self.f_sma_given_Rp2 = lambda a, beta=self.beta[1], m=m, C=self.Ca[1], \
+            smaknee=self.smaknee: self.dist_sma_given_radius(a, beta, m, C, smaknee)
+            
+        
+    def generate_NEA_planets(self):
+        """Helper function to generate the NEA planets within the specified ranges.
+
+        Returns
+        -------
+        pandas.DataFrame
+        A pandas DataFrame containing the exoplanet data from the NEA, filtered by the specified ranges.
+        """
+
+
+        archive = nea.getExoplanetArchivePSCP()
+
+        # Limit the data to the ranges defined in NEA_ranges
+        for key, value in self.NEA_ranges.items():
+            archive = archive[(archive[key] >= value[0]) & (archive[key] <= value[1])]
+
+        return archive.reset_index(drop=True)
+
+
+    def infer_PDF(self, array, array_range, data):
+        """Given an array of values, a range for those values, and data,
+        this function approximates the probability density function (PDF) for the data
+        within the specified range. It uses a Gaussian kernel density estimation (KDE) to
+        compute the PDF values for the input data.
+
+        Parameters
+        ----------
+        array : array_like
+            An array of values for which the PDF is to be computed.
+        array_range : list or tuple
+            A range (min, max) within which the PDF is computed.
+        data : array_like
+            The data from which the PDF is estimated.
+
+        Returns
+        -------
+        f : numpy.ndarray
+            An array containing the estimated PDF values.
+        """
+
+        array = np.array(array, ndmin=1)
+        
+        f = np.zeros(array.shape)
+
+        mask = np.array((array >= array_range[0]) & (array <= array_range[1]), ndmin=1)
+
+        kde = gaussian_kde(data)
+
+        f[mask] = kde.evaluate(array[mask])
+
+        return f
+    
+    
+    def gen_radius_sma(self, n):
+        N_sampled = n - self.N_nea
+
+        dist_Rp = lambda Rp: (n / N_sampled) * self.dist_radius(Rp) - (self.N_nea / N_sampled) * self.dist_rad_nea(Rp)
+
+        # Sample radius
+        Rp = InverseTransformSampler(dist_Rp, self.Rprange[0].value, self.Rprange[1].value)(N_sampled)
+        a = np.zeros_like(Rp)
+
+        # Get modified density function for semi-major axis
+        dist_a_given_Rp1 = lambda a: (n / len(Rp[Rp < self.Rplim[1]])) * self.f_sma_given_Rp1(a) - \
+              (self.N_nea / len(Rp[Rp < self.Rplim[1]])) * self.dist_sma_nea(a)
+        dist_a_given_Rp2 = lambda a: (n / len(Rp[Rp >= self.Rplim[1]])) * self.f_sma_given_Rp2(a) - \
+                (self.N_nea / len(Rp[Rp >= self.Rplim[1]])) * self.dist_sma_nea(a)
+
+        # Sample semi-major axis given radius
+        a[Rp < self.Rplim[1]] = InverseTransformSampler(dist_a_given_Rp1, self.arange[0].value, self.arange[1].value)(len(Rp[Rp < self.Rplim[1]]))
+        a[Rp >= self.Rplim[1]] = InverseTransformSampler(dist_a_given_Rp2, self.arange[0].value, self.arange[1].value)(len(Rp[Rp >= self.Rplim[1]]))
+
+        # Stack the data
+        a = np.concatenate((self.archive['pl_orbsmax'], a))
+        Rp = np.concatenate((self.archive['pl_rade'], Rp))
+
+        return Rp * u.earthRad, a * u.AU
+        
+    
+    def gen_plan_params(self, n):
+
+        n = self.gen_input_check(n)
+        
+        Rp, a = self.gen_radius_sma(n)
+
+        C1 = np.exp(-self.erange[0] ** 2 / (2.0 * self.esigma**2))
+        ar = self.arange.to("AU").value
+        if self.constrainOrbits:
+            # restrict semi-major axis limits
+            arcon = np.array(
+                [ar[0] / (1.0 - self.erange[0]), ar[1] / (1.0 + self.erange[0])]
+            )
+            # clip sma values to sma range
+            sma = np.clip(a.to("AU").value, arcon[0], arcon[1])
+            # upper limit for eccentricity given sma
+            elim = np.zeros(len(sma))
+            amean = np.mean(ar)
+            elim[sma <= amean] = 1.0 - ar[0] / sma[sma <= amean]
+            elim[sma > amean] = ar[1] / sma[sma > amean] - 1.0
+            elim[elim > self.erange[1]] = self.erange[1]
+            elim[elim < self.erange[0]] = self.erange[0]
+            # additional constant
+            C2 = C1 - np.exp(-(elim**2) / (2.0 * self.esigma**2))
+            a = sma * u.AU
+        else:
+            C2 = self.enorm
+        
+        e = self.esigma * np.sqrt(-2.0 * np.log(C1 - C2 * np.random.uniform(size=n)))
+
+        # generate albedo from semi-major axis
+        p = self.PlanetPhysicalModel.calc_albedo_from_sma(a, self.prange)
+
+        return a, e, p, Rp
---
+++ EXOSIMS/SimulatedUniverse/PDFSampledUniverse.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+from EXOSIMS.Prototypes.SimulatedUniverse import SimulatedUniverse
+
+
+class PDFSampledUniverse(SimulatedUniverse):
+    """Simulated Universe module based on SAG13 Planet Population module.
+    """
+
+    def __init__(self, earthPF=False, **specs):
+        self.earthPF = earthPF
+        SimulatedUniverse.__init__(self, **specs)
+        self._outspec["earthPF"] = self.earthPF
+
+    def gen_physical_properties(self, **specs):
+        """Generating universe that includes both real exoplanets from the NASA Exoplanet Archive (NEA) and synthetic planets
+        sampled from a modified density function to match the SAG13 occurrence rates.
+
+        All parameters except for albedo and mass are sampled, while those are
+        calculated via the physical model.
+
+        """
+
+        PPop = self.PlanetPopulation
+        PPMod = self.PlanetPhysicalModel
+        TL = self.TargetList
+
+
+        # 1. First check how many host stars acually have planets
+        # 2. Generate a the target system sample with the given eta
+        # 3. Randomly remove Nplanets that are known from the sample list
+        # i.e. randomly select N_tot - N_NEA of the N_tot sampled planets
+
+        targetSystems = np.random.poisson(lam=PPop.eta, size=TL.nStars)
+        planinds = []
+        starinds = []
+        planmask = []
+        for j, n in enumerate(targetSystems):
+            # Check if the star has known planets
+            tmp = np.where(PPop.hostname == TL.Name[j])[0]
+            if len(tmp) >= n:
+                planinds = np.hstack((planinds, tmp))
+                starinds = np.hstack((starinds, [j] * len(tmp)))
+                planmask = np.hstack((planmask, np.ones(len(tmp), dtype=bool)))
+            else:
+                planinds = np.hstack((planinds, tmp))
+                starinds = np.hstack((starinds, [j] * n))
+                planmask = np.hstack((planmask, [True] * len(tmp) + [False] * (n - len(tmp))))
+        
+        planinds = planinds.astype(int)
+        starinds = starinds.astype(int)
+
+        self.plan2star = starinds
+        self.sInds = np.unique(self.plan2star)
+        self.nPlans = len(starinds)
+        N_NEA = len(planinds)
+
+        PPop.planmask = planmask
+
+        N_sampled = self.nPlans - N_NEA
+
+        # sample all of the orbital and physical parameters
+        self.I, self.O, self.w = PPop.gen_angles(
+            self.nPlans,
+            commonSystemPlane=self.commonSystemPlane,
+            commonSystemPlaneParams=self.commonSystemPlaneParams,
+        )
+        self.setup_system_planes()
+        self.a, self.e, self.p, self.Rp = PPop.gen_plan_params(self.nPlans)
+        if PPop.scaleOrbits:
+            self.a *= np.sqrt(TL.L[self.plan2star])
+        self.gen_M0()  # initial mean anomaly
+        self.Mp = PPMod.calc_mass_from_radius(self.Rp)  # mass
+
+        # Use Earth Phase Function
+        if self.earthPF:
+            self.phiIndex = (
+                np.ones(self.nPlans, dtype=int) * 2
+            )  # Used to switch select specific phase function for each planet
+        else:
+            self.phiIndex = np.asarray(
+                []
+            )  # Used to switch select specific phase function for each planet
+        ZL = self.ZodiacalLight
+        if self.commonSystemnEZ:
+            # Assign the same nEZ to all planets in the system
+            self.nEZ = ZL.gen_systemnEZ(TL.nStars)[self.plan2star]
+        else:
+            # Assign a unique nEZ to each planet
+            self.nEZ = ZL.gen_systemnEZ(self.nPlans)
